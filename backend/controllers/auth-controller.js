c7f42047a1e4120ea8690ad59320a5966ae4eb22
Truong Duc Hoang <tantiennguyen2404@gmail.com>
2025-12-06 15:48:53 +0700

    Done send and receive a verification email

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import Verification from "../models/verification.js";
+import { sendEmail } from "../libs/send-email.js";
 
 const registerUser = async (request, response) => {
   try {
@@ -23,12 +26,37 @@ const registerUser = async (request, response) => {
       name,
     });
 
-    response
-      .status(201)
-      .json({
-        message:
-          "Verification email sent to your email. Please check and verify your email to complete registration.",
+    const verificationToken = jwt.sign(
+      { userId: newUser._id, property: "email-verification" },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    await Verification.create({
+      userId: newUser._id,
+      token: verificationToken,
+      expiresAt: new Date(Date.now() + 1 * 60 * 60 * 1000),
+    });
+
+    //send email
+    const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}`;
+
+    const emailBody = `<p>Click <a href="${verificationLink}">here</a> to verify your email</p>`;
+
+    const emailSubject = "Verify your email address";
+
+    const isEmailSent = await sendEmail(email, emailSubject, emailBody);
+
+    if (!isEmailSent) {
+      return response.status(500).json({
+        message: "Failed to send verification email. Please try again later.",
       });
+    }
+
+    response.status(201).json({
+      message:
+        "Verification email sent to your email. Please check and verify your email to complete registration.",
+    });
   } catch (error) {
     console.log(error);
     response.status(500).json({
---
+++ backend/libs/send-email.js
@@ -0,0 +1,26 @@
+import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+
+const fromEmail = process.env.FROM_EMAIL;
+
+export const sendEmail = async (to, subject, html) => {
+  const msg = {
+    to,
+    from: `OU Task Management <${fromEmail}>`,
+    subject,
+    html,
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent successfully");
+    return true;
+  } catch (error) {
+    console.error("Error sending email: ", error);
+    return false;
+  }
+};
